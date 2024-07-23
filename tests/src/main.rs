76f101e783a4be1a5df2c7244924a67097047912
Amos Wenger <amos@bearcove.net>
2024-07-23 19:26:11 +0200

    Mh

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -380,11 +380,6 @@ fn run_tests() -> io::Result<()> {
         let env_vars =
             env_vars.with_additional_library_path(additional_path.to_string_lossy().into_owned());
 
-        println!("\nEnvironment Variables Summary:");
-        env_vars.each_kv(|key, value| {
-            println!("{}: {}", key, value);
-        });
-
         let (success, output) = run_command(test.run_command, &env_vars)?;
 
         match (test.expected_result, success) {
